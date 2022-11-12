a8477696416f02f03e30896dcdd5b9d371270362
leanhhuy0709 <78292068+leanhhuy0709@users.noreply.github.com>
2022-11-12 12:07:52 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
 const naviBar = document.getElementById('naviBar')
+const naviBar2 = document.getElementById('naviBar2')
 const foodList = document.getElementById('foodList')
 const Cart = document.getElementById('cart')
 const Payment = document.getElementById('payment')
@@ -13,9 +14,9 @@ function renderNaviBar()
         React.createElement('div', {className: 'w3-bar w3-border w3-cyan w3-top'}, 
             React.createElement('button', {className: 'w3-bar-item w3-button', 
                 onClick: function() {filter('')}, style: {fontWeight: 'bold'}}, 'BKU FOOD'),
-            React.createElement('a', {className: 'w3-bar-item w3-button w3-right', href: 'login.html', 
+            React.createElement('a', {className: 'w3-bar-item w3-button w3-right', href: '../index.html', 
                 style: {fontWeight: 'bold', display: (!isUser)?'block':'none'}}, 'Login'),
-            React.createElement('a', {className: 'w3-bar-item w3-button w3-right', href: 'login.html', 
+            React.createElement('a', {className: 'w3-bar-item w3-button w3-right', href: '../index.html', 
                 style: {fontWeight: 'bold', display: (isUser)?'block':'none'}}, 'Logout'),
             
             React.createElement('a', {className: 'w3-bar-item w3-button w3-right', style: {fontWeight: 'bold'},
@@ -26,14 +27,14 @@ function renderNaviBar()
                 style: {fontWeight: 'bold', display: (isUser)?'block':'none'}}, 'Level ' + user['Rank']),
             React.createElement('a', {className: 'w3-bar-item w3-right', 
                 style: {fontWeight: 'bold', display: (isUser)?'block':'none'}}, user['Name']),
+            React.createElement('input', {className: 'w3-left hw-input', id: 'naviBar_search', placeholder: "Enter Food ID or Food Name"}),
+            React.createElement('button', {className: 'w3-left w3-button', style: {fontWeight: 'bold'}, 
+                onClick: function() {search(String(document.getElementById('naviBar_search').value), 'food_')},
+                }, 
+                React.createElement('i', {className: 'fa fa-search'})),
         )
     
     ReactDOM.render(tmp, naviBar)
-    /*<div class="w3-bar w3-border w3-cyan w3-top">
-        <button class="w3-bar-item w3-button" onclick = "renderFoodList()"><b>BKU FOOD</b></button>
-        <a href="login.html" class="w3-bar-item w3-button w3-right"><b>Login</b></a>
-        <a class="w3-bar-item w3-button w3-right" onclick = "renderCart()"><b>Cart <i class="fa fa-shopping-cart"></i> </b></a>
-    </div> */
 
 }
 
@@ -46,13 +47,13 @@ function renderFoodList() {
         const tmp = React.createElement(
             'div',
             {
-                className: 'w3-card w3-left w3-center hw-food-item',
+                className: 'w3-card w3-center hw-food-item',
                 id: 'food_' + i.toString(),
             },
             React.createElement(
                 'img',
                 {
-                    src: './images/' + food[i]['picture'],
+                    src: '../images/' + food[i]['picture'],
                     style: {width:'100%', height:'180px'},
                     alt: food[i]['name'],
                 }
@@ -75,7 +76,7 @@ function renderFoodList() {
         
     }
 
-    const foodItem = React.createElement('div', null, 
+    const foodItem = React.createElement('div', {className: 'w3-center'}, 
         React.createElement('div', {className: 'w3-container w3-center'}, 
             React.createElement('h1', {style: {fontWeight: 'bold'}}, 'FOOD LIST')
             ), foodItemArr)
@@ -95,13 +96,13 @@ function renderCart() {
         const tmp = React.createElement(
             'div',
             {
-                className: 'w3-card w3-left w3-center hw-food-item',
-                style: {display: (cart[i] > 0)? 'block':'none'}
+                className: 'w3-card w3-center hw-food-item',
+                style: {display: (cart[i] > 0)? 'inline-block':'none'}
             },
             React.createElement(
                 'img',
                 {
-                    src: './images/' + food[i]['picture'],
+                    src: '../images/' + food[i]['picture'],
                     style: {width:'100%', height:'180px'},
                     alt: food[i]['name'],
                 }
@@ -151,7 +152,7 @@ function renderPayment()
     const OrderElement = React.createElement('div', {}, 
         React.createElement('div', {className: 'w3-container w3-center'},
             React.createElement('button', {className: 'w3-button w3-black w3-margin', 
-                onClick: function() {document.getElementById('payment_modal').style.display='block'}}, 'Payment')),
+                onClick: function() {document.getElementById('payment_modal').style.display='inline-block'}}, 'Payment')),
         React.createElement('div', {id: 'payment_modal', className: 'w3-modal'},
             React.createElement('div', {className: 'w3-modal-content w3-card-4'}, 
                 React.createElement('header', {className: 'w3-container w3-cyan'}, 
@@ -185,6 +186,111 @@ function renderPayment()
     
 
     ReactDOM.render(OrderElement, Payment)
+}
+//Edit page
+function renderEditPage()
+{
+    const editPage = document.getElementById('editPage');
+    if (editPage == null) return;
+    const eP = React.createElement('div', {className: 'w3-center'}, 
+        React.createElement('div', {className: 'w3-container w3-center'}, 
+            React.createElement('h1', {style: {fontWeight: 'bold'}}, 'EDIT PAGE')),
+        
+        React.createElement('input', {name: 'filename', className: 'hw-input', type: 'file', required: 'required', placeholder: "Enter Food Picture"},),
+        React.createElement('br'),
+        React.createElement('input', {name: 'foodName', className: 'hw-input', required: 'required', placeholder: "Enter Food Name"},),
+        React.createElement('br'),
+        React.createElement('input', {name: 'price', className: 'hw-input', required: 'required', placeholder: "Enter Price (unit: nghìn đồng)"},),
+        React.createElement('br'),
+        React.createElement('br'),
+        React.createElement('button', {className: 'w3-button w3-blue', type: 'submit'}, 'Add food'),
+        )
+    ReactDOM.render(eP, editPage);
+}
+renderEditPage()
+renderRemovePage()
+//Remove Page
+function renderRemovePage()
+{
+    const removePage = document.getElementById('removePage');
+    if (removePage == null) return;
+    var foodItemArr = []
+
+    for(let i = 0; i < size; i++) {
+
+        const tmp = React.createElement(
+            'div',
+            {
+                id: 'remove_food_' + i.toString(),
+                className: 'w3-card w3-center hw-food-item',
+                style: {display: 'none'}
+            },
+            React.createElement(
+                'img',
+                {
+                    src: '../images/' + food[i]['picture'],
+                    style: {width:'100%', height:'180px'},
+                    alt: food[i]['name'],
+                }
+            ),
+            React.createElement(
+                'div',
+                {
+                    className: 'w3-container'
+                },
+                React.createElement('h4', null, React.createElement('b', null, 'ID: ' + food[i]['id'])),
+                React.createElement('h4', null, React.createElement('b', null, food[i]['name'])),
+                React.createElement('h4', null, toMoney(food[i]['price'])),
+                React.createElement('button', {
+                    className: 'w3-button w3-blue w3-margin', 
+                    onClick: function()
+                    { 
+                        document.getElementById('remove_input').value = i;
+                    }, 
+                    type: 'submit'
+                }, 'Remove'),
+            )
+        )
+        foodItemArr.push(tmp)
+    }
+
+    const rP = React.createElement('form', {className: 'w3-center', type: 'post'}, 
+        React.createElement('div', {className: 'w3-container w3-center'}, 
+            React.createElement('h1', {style: {fontWeight: 'bold'}}, 'REMOVE ITEM')),
+        React.createElement('input', {id: 'remove_input', name: 'remove_id', className: 'hw-input', 
+            required: 'required', placeholder: "Enter Food ID or Food Name"},),
+        React.createElement('br'),
+        foodItemArr,
+        React.createElement('br'),
+        React.createElement('button', {className: 'w3-button w3-blue w3-margin', type: 'button', 
+            onClick: function() {     
+                search(document.getElementById('remove_input').value, 'remove_food_')
+            }
+            }, 'Search')
+        )
+
+    
+    ReactDOM.render(rP, removePage);
+
+}
+
+//Add Promotion
+renderAddPromotion()
+
+function renderAddPromotion()
+{
+    const editPage = document.getElementById('addPromotion');
+    if (editPage == null) return;
+    const eP = React.createElement('div', {className: 'w3-center'}, 
+        React.createElement('div', {className: 'w3-container w3-center'}, 
+            React.createElement('h1', {style: {fontWeight: 'bold'}}, 'ADD PROMOTION')),
+        React.createElement('br'),
+        React.createElement('input', {name: 'promotion_code', className: 'hw-input', required: 'required', 
+            placeholder: "Enter Promotion Code", pattern: '.{8,}', title: "Eight or more characters"},),
+        React.createElement('br'),
+        React.createElement('button', {className: 'w3-button w3-blue', type: 'submit'}, 'Add'),
+        )
+    ReactDOM.render(eP, editPage);
 }
 
 function f(money)
@@ -283,9 +389,37 @@ function filter(keyword)
         const tmp = document.getElementById('food_' + i.toString())
         if(!tmp) {continue};
         if (food[i]['type'].search(keyword) != -1)
-            tmp.style.display = 'block';
+            tmp.style.display = 'inline-block';
         else
             tmp.style.display = 'none';
+    }
+}
+
+function search(input, formula_id)
+{
+    renderFoodList()
+
+    var showList = []
+
+    for (let i = 0; i < size; i++)
+    {
+        let tmp = (food[i]['name'].toLowerCase()).search((input.toLowerCase()));
+        if (input == food[i]['id'].toString() 
+        || tmp != -1)
+            showList.push(i);
+    }
+    let tmp;
+    for (let i = 0; i < size; i++)
+    {
+        tmp = document.getElementById(formula_id + i.toString());
+        if(!tmp) {continue};
+        tmp.style.display = 'none';
+    }
+    for (let i = 0; i < showList.length; i++)
+    {
+        tmp = document.getElementById(formula_id + showList[i].toString());
+        if(!tmp) {continue};
+        tmp.style.display = 'inline-block';
     }
 }
 
